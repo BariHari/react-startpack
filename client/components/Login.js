158e768cc5e49f88dc310e1f5e222750b152e777
BariHari <saleena.beharry@me.com>
2017-09-09 22:12:29 -0400

    added div for component

--- client/components/Login.js
+++ client/components/Login.js
@@ -3,8 +3,11 @@ import React, {PropTypes} from 'react';
 export default class Login extends React.Component {
     render() {
         return (
-            <h1>
-                Hello Happy Dog
-            </h1>);
+        <div>
+            <h1>Hello Happy Dog</h1>
+            <p>helloooo</p>
+        </div>
+        );
+
     }
 }
