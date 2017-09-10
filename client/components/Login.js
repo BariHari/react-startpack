12ae97fd85d2f6dff7357cbef15f68461f7184c6
BariHari <saleena.beharry@me.com>
2017-09-09 21:55:02 -0400

    Added second component

---
+++ client/components/Login.js
@@ -0,0 +1,10 @@
+import React, {PropTypes} from 'react';
+
+export default class Login extends React.Component {
+    render() {
+        return (
+            <h1>
+                Hello Happy Dog
+            </h1>);
+    }
+}
